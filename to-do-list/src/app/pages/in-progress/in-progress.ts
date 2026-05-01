38a32fca18885efb1b7679ce10a4b352cb579030
Bozhidar Zagorov <bozhidarzagorov@gmail.com>
2026-05-01 21:04:13 +0300

    in progress auth guard and tasks

--- to-do-list/src/app/pages/in-progress/in-progress.ts
+++ to-do-list/src/app/pages/in-progress/in-progress.ts
@@ -1,9 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Auths } from '../../auth/auth';
+import { AsyncPipe, CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { TodoService } from '../../todos/todo';
+import { FormsModule } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-in-progress',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, AsyncPipe, RouterLink, FormsModule],
   templateUrl: './in-progress.html',
   styleUrl: './in-progress.scss',
 })
-export class InProgress {}
+export class InProgress implements OnInit {
+
+  newTodo = '';
+  todos$!: Observable<any[]>;
+
+  constructor(
+    public auth: Auths,
+    private todoService: TodoService
+  ) {}
+
+  ngOnInit() {
+    this.todos$ = this.todoService.getTodos(false);
+  }
+
+  addTodo() {
+    if (!this.newTodo.trim()) return;
+
+    this.todoService.addTodo(this.newTodo);
+    this.newTodo = '';
+  }
+
+  completeTodo(id: string) {
+    this.todoService.toggleTodo(id, true);
+  }
+}
