27348ed8017f03172afe1c9748732a3e844e3861
Bozhidar Zagorov <bozhidarzagorov@gmail.com>
2026-05-01 21:04:21 +0300

    completed tasks

--- to-do-list/src/app/pages/completed/completed.ts
+++ to-do-list/src/app/pages/completed/completed.ts
@@ -1,9 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Auths } from '../../auth/auth';
+import { AsyncPipe, CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { TodoService } from '../../todos/todo';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-completed',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, AsyncPipe, RouterLink],
   templateUrl: './completed.html',
   styleUrl: './completed.scss',
 })
-export class Completed {}
+export class Completed implements OnInit {
+
+  todos$!: Observable<any[]>;
+
+  constructor(
+    public auth: Auths,
+    private todoService: TodoService
+  ) {}
+
+  ngOnInit() {
+    this.todos$ = this.todoService.getTodos(true);
+  }
+
+  undoTodo(id: string) {
+    this.todoService.toggleTodo(id, false);
+  }
+}
