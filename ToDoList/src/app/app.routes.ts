4f32d538e08095544932985e3f6821b774580e90
Bozhidar Zagorov <bozhidarzagorov@gmail.com>
2026-05-01 15:32:53 +0300

    add routes to app

--- ToDoList/src/app/app.routes.ts
+++ ToDoList/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Login } from './pages/login/login';
+import { Register } from './pages/register/register';
+import { InProgress } from './pages/in-progress/in-progress';
+import { Completed } from './pages/completed/completed';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: Login },
+  { path: 'register', component: Register },
+  { path: 'in-progress', component: InProgress },
+  { path: 'completed', component: Completed },
+  { path: '', redirectTo: 'in-progress', pathMatch: 'full' }
+];
