24e151f4a9a022f4ce2b1e75cf4d6d1842c76af4
Bozhidar Zagorov <bozhidarzagorov@gmail.com>
2026-05-01 18:04:38 +0300

    firebase auth register

---
+++ to-do-list/src/app/auth/register/register.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { Auths } from '../auth';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-register',
+  imports: [FormsModule],
+  templateUrl: './register.html'
+})
+export class RegisterComponent {
+
+  email = '';
+  password = '';
+
+  constructor(private auth: Auths, private router: Router) {}
+
+  register() {
+    this.auth.register(this.email, this.password)
+      .then(() => this.router.navigate(['/login']))
+      .catch(err => console.error(err));
+  }
+}
