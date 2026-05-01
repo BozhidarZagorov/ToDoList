0fce01b4614fbdb132a81f93e11734a57b9ada85
Bozhidar Zagorov <bozhidarzagorov@gmail.com>
2026-05-01 18:05:23 +0300

    login and register route

--- to-do-list/src/app/app.routes.ts
+++ to-do-list/src/app/app.routes.ts
@@ -1,12 +1,16 @@
 import { Routes } from '@angular/router';
-import { Login } from './pages/login/login';
-import { Register } from './pages/register/register';
+// import { Login } from './pages/login/login';
+// import { Register } from './pages/register/register';  // no longer in use
 import { InProgress } from './pages/in-progress/in-progress';
 import { Completed } from './pages/completed/completed';
 
+import { LoginComponent } from './auth/login/login';
+import { RegisterComponent } from './auth/register/register';
+
+
 export const routes: Routes = [
-  { path: 'login', component: Login },
-  { path: 'register', component: Register },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
   { path: 'in-progress', component: InProgress },
   { path: 'completed', component: Completed },
   { path: '', redirectTo: 'in-progress', pathMatch: 'full' }
