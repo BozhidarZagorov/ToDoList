d8c0280d82678549ad9ac7df48a429330e823238
Bozhidar Zagorov <bozhidarzagorov@gmail.com>
2026-05-01 21:04:50 +0300

    app routes

--- to-do-list/src/app/app.routes.ts
+++ to-do-list/src/app/app.routes.ts
@@ -7,11 +7,28 @@ import { Completed } from './pages/completed/completed';
 import { LoginComponent } from './auth/login/login';
 import { RegisterComponent } from './auth/register/register';
 
+import { authGuard } from './auth/auth-guard'
+import { guestGuard } from './auth/guest-guard';
+
+
+// export const routes: Routes = [
+//   { path: 'login', component: LoginComponent },
+//   { path: 'register', component: RegisterComponent },
+//   { path: 'in-progress', component: InProgress },
+//   { path: 'completed', component: Completed },
+//   { path: '', redirectTo: 'in-progress', pathMatch: 'full' }
+// ];
 
 export const routes: Routes = [
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
-  { path: 'in-progress', component: InProgress },
-  { path: 'completed', component: Completed },
-  { path: '', redirectTo: 'in-progress', pathMatch: 'full' }
+  { path: 'login', canActivate: [guestGuard], loadComponent: () => import('./auth/login/login').then(m => m.LoginComponent) },
+  { path: 'register', canActivate: [guestGuard], loadComponent: () => import('./auth/register/register').then(m => m.RegisterComponent) },
+
+  {
+    path: 'in-progress',
+    loadComponent: () => import('./pages/in-progress/in-progress').then(m => m.InProgress)
+  },
+  {
+    path: 'completed',
+    loadComponent: () => import('./pages/completed/completed').then(m => m.Completed)
+  }
 ];
