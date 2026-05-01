abab53de56f16b88767cbdb6e2ccecb246c54666
Bozhidar Zagorov <bozhidarzagorov@gmail.com>
2026-05-01 18:04:25 +0300

    firebase auth login

---
+++ to-do-list/src/app/auth/login/login.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { Auths } from '../auth';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  imports: [FormsModule],
+  templateUrl: './login.html',
+})
+export class LoginComponent {
+
+  email = '';
+  password = '';
+
+  constructor(private auth: Auths, private router: Router) {}
+
+  login() {
+    this.auth.login(this.email, this.password)
+      .then(() => this.router.navigate(['/']))
+      .catch(err => console.error(err));
+  }
+}
