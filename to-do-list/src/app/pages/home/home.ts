d73bf68ae81f7bc7079437fb57f9b60c2d14347e
Bozhidar Zagorov <bozhidarzagorov@gmail.com>
2026-05-02 18:05:20 +0300

    home page and some basic style

--- to-do-list/src/app/pages/home/home.ts
+++ to-do-list/src/app/pages/home/home.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
+import { CommonModule, AsyncPipe } from '@angular/common';
+import { Auths } from '../../auth/auth';
+import { TodoService } from '../../todos/todo';
+import { map } from 'rxjs/operators';
+import { inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule, AsyncPipe, RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
 export class Home {
+  private auth = inject(Auths);
+  private todoService = inject(TodoService);
 
+  user$ = this.auth.user$;
+
+  inProgressCount$ = this.todoService.getTodos(false).pipe(
+    map(todos => todos.length)
+  );
+
+  completedCount$ = this.todoService.getTodos(true).pipe(
+    map(todos => todos.length)
+  );
 }
