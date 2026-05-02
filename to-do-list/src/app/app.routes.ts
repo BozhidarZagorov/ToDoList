d720ff74abd105b2993c1ed585a7c5a18ac8dfcb
Bozhidar Zagorov <bozhidarzagorov@gmail.com>
2026-05-02 15:55:12 +0300

    add home to route

--- to-do-list/src/app/app.routes.ts
+++ to-do-list/src/app/app.routes.ts
@@ -6,9 +6,10 @@ import { Completed } from './pages/completed/completed';
 
 import { LoginComponent } from './auth/login/login';
 import { RegisterComponent } from './auth/register/register';
-
 import { authGuard } from './auth/auth-guard'
+
 import { guestGuard } from './auth/guest-guard';
+import { Home } from './pages/home/home';
 
 
 // export const routes: Routes = [
@@ -20,6 +21,7 @@ import { guestGuard } from './auth/guest-guard';
 // ];
 
 export const routes: Routes = [
+  { path: '', component: Home },
   { path: 'login', canActivate: [guestGuard], loadComponent: () => import('./auth/login/login').then(m => m.LoginComponent) },
   { path: 'register', canActivate: [guestGuard], loadComponent: () => import('./auth/register/register').then(m => m.RegisterComponent) },
 
