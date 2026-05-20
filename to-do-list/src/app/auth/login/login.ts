97c878cfc9a88105ca4453a05e02580b3c3fe2fc
Bozhidar Zagorov <bozhidarzagorov@gmail.com>
2026-05-20 20:41:47 +0300

    add style import to login and register

--- to-do-list/src/app/auth/login/login.ts
+++ to-do-list/src/app/auth/login/login.ts
@@ -7,6 +7,7 @@ import { FormsModule } from '@angular/forms';
   selector: 'app-login',
   imports: [FormsModule],
   templateUrl: './login.html',
+  styleUrl: './login.scss'
 })
 export class LoginComponent {
 
--- to-do-list/src/app/auth/register/register.ts
+++ to-do-list/src/app/auth/register/register.ts
@@ -6,7 +6,8 @@ import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-register',
   imports: [FormsModule],
-  templateUrl: './register.html'
+  templateUrl: './register.html',
+  styleUrl: './register.scss'
 })
 export class RegisterComponent {
 
