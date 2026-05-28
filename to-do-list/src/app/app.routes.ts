b2703d9483b566f75b2fa62a4948cb477c42d263
Bozhidar Zagorov <bozhidarzagorov@gmail.com>
2026-05-28 22:48:34 +0300

    remove unused routes and imports

--- to-do-list/src/app/app.routes.ts
+++ to-do-list/src/app/app.routes.ts
@@ -1,24 +1,8 @@
 import { Routes } from '@angular/router';
-// import { Login } from './pages/login/login';
-// import { Register } from './pages/register/register';  // no longer in use
-import { InProgress } from './pages/in-progress/in-progress';
-import { Completed } from './pages/completed/completed';
-
-import { LoginComponent } from './auth/login/login';
-import { RegisterComponent } from './auth/register/register';
-import { authGuard } from './auth/auth-guard'
 
 import { guestGuard } from './auth/guest-guard';
 import { Home } from './pages/home/home';
 
-
-// export const routes: Routes = [
-//   { path: 'login', component: LoginComponent },
-//   { path: 'register', component: RegisterComponent },
-//   { path: 'in-progress', component: InProgress },
-//   { path: 'completed', component: Completed },
-//   { path: '', redirectTo: 'in-progress', pathMatch: 'full' }
-// ];
 
 export const routes: Routes = [
   { path: '', component: Home },
