540b1aeae3fc21c56a0ba78574982a91a5d38769
Bozhidar Zagorov <bozhidarzagorov@gmail.com>
2026-05-28 22:58:36 +0300

    ,envs firebase

---
+++ to-do-list/src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyD106KwQBVForHg5_-UZGgHQngZzfulZb0",
+    authDomain: "mytodolisted.firebaseapp.com",
+    projectId: "mytodolisted",
+    storageBucket: "mytodolisted.firebasestorage.app",
+    messagingSenderId: "109583468170",
+    appId: "1:109583468170:web:d092e4f3987209a9750f73"
+  }
+};
---
+++ to-do-list/src/fiebaseconfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD106KwQBVForHg5_-UZGgHQngZzfulZb0",
+  authDomain: "mytodolisted.firebaseapp.com",
+  projectId: "mytodolisted",
+  storageBucket: "mytodolisted.firebasestorage.app",
+  messagingSenderId: "109583468170",
+  appId: "1:109583468170:web:d092e4f3987209a9750f73"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
